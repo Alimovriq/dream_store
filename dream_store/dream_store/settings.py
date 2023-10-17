8d19cc94c3cb73ad7c2933a2dd797a014faf7254
Alimov Rinat <r-alimov@ya.ru>
2023-10-17 20:15:44 +0300

    Full User and UserProfile models with admin panel and email registraion

--- dream_store/dream_store/settings.py
+++ dream_store/dream_store/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'products',
     'news',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -105,6 +106,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = 'users.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
