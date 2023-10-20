451b576930abcf710b2edee7ff8c1f9c3a30aeda
Alimov Rinat <r-alimov@ya.ru>
2023-10-20 22:38:06 +0300

    image field base64 for products in serializers, post method for products views

--- dream_store/api/serializers/products_serializers.py
+++ dream_store/api/serializers/products_serializers.py
@@ -1,3 +1,8 @@
+import base64
+
+from django.core.files.base import ContentFile
+from rest_framework import serializers
+
 from products.models import (
     Products, ProductQuantity,
     Shop_basket, Shop_basket_items,
@@ -5,7 +10,22 @@ from products.models import (
     Categories)
 
 
-from rest_framework import serializers
+class Base64ImageField(serializers.ImageField):
+    """
+    Кастомный сериализатор для декодирования изображений.
+    """
+
+    def to_internal_value(self, data):
+        """
+        Метод работает при поступлении запроса.
+        """
+
+        if isinstance(data, str) and data.startswith('data:image'):
+            format, imgstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+
+        return super().to_internal_value(data)
 
 
 class ProductsSerializer(serializers.Serializer):
@@ -18,8 +38,34 @@ class ProductsSerializer(serializers.Serializer):
     price = serializers.DecimalField(max_digits=10,
                                      decimal_places=2,
                                      required=True)
-    image = serializers.ImageField(required=False)
+    image = Base64ImageField(required=False)
     description = serializers.CharField(required=False, allow_blank=True,
                                         max_length=500)
-    category = serializers.PrimaryKeyRelatedField(read_only=True)
+    category = serializers.SlugRelatedField(
+        queryset=Categories.objects.all(), slug_field='name')
     slug = serializers.SlugField(required=True, max_length=50)
+
+    def create(self, validated_data):
+        """
+        Метод для создания и возврата объекта экземпляра от
+        входящих данных.
+        """
+
+        return Products.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Метод для обновления и возврата объекта экземпляра от
+        входящих данных
+        """
+
+        instance.name = validated_data.get('name', instance.name)
+        instance.price = validated_data.get('price', instance.price)
+        instance.image = validated_data.get('image', instance.image)
+        instance.description = validated_data.get(
+            'description', instance.description)
+        instance.category = validated_data.get(
+            'category', instance.category)
+        instance.slug = validated_data.get('slug', instance.slug)
+        instance.save()
+        return instance
