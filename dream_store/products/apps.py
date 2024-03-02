c54e53bed6310f576ce9b02d0e4f02a475d32da3
Alimov Rinat <r-alimov@ya.ru>
2024-03-03 00:58:26 +0300

    deleted unused comments and added info response if orders are empty

--- dream_store/products/apps.py
+++ dream_store/products/apps.py
@@ -5,6 +5,3 @@ class ProductsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'products'
     verbose_name = 'Управление товарами'
-
-    # def ready(self) -> None:
-    #     import products.signals
